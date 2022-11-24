cdd125fcc4b77e5abcb4d3e98715d63ce7282b85
PabloNA97 <p.navarro.acero@gmail.com>
2022-11-24 19:54:51 +0100

    Additional flags

--- 2.Clustering_CavityAnalysis/run_local.sh
+++ 2.Clustering_CavityAnalysis/run_local.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# rm -rf output
-python biobb_clustering_cavity_analysis.py --config input.yml --until all
+TRAJ_PATH=/home/nbd-pablo/repos/eucanshare_wfs/1.MD_setup_mutation/output/step24_trjcat/all_trajectories.trr
+TOP_PATH=/home/nbd-pablo/repos/eucanshare_wfs/1.MD_setup_mutation/output/step23_dry/imaged_structure.gro 
+CLUSTERING_PATH=/home/nbd-pablo/Documents/2022_ENSEM/3.P53/MD_Trajectories/3ts8/CLUSTER/selection2/low_num_clusters/HIERAGGLO/reps
+
+python biobb_clustering_cavity_analysis.py --input-traj $TRAJ_PATH --input-top $TOP_PATH --config input.yml
+
+# python biobb_clustering_cavity_analysis.py --input-clust $CLUSTERING_PATH --config input.yml
