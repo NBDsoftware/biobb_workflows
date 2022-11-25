aa904c53dca7a5cd80d6dfd2f893f3707b6dc471
PabloNA97 <p.navarro.acero@gmail.com>
2022-11-25 16:39:37 +0100

    Checkpoint :)

--- 4.Pocket_VS/biobb_pocket_vs.py
+++ 4.Pocket_VS/biobb_pocket_vs.py
@@ -12,32 +12,40 @@ import argparse
 import shutil
 from pathlib import Path
 
-from biobb_docking_htvs import docking_htvs
+from biobb_docking_htvs import main_wf as docking_htvs
 from biobb_common.configuration import settings
 from biobb_common.tools import file_utils as fu
 from biobb_vs.fpocket.fpocket_run import fpocket_run
 from biobb_vs.fpocket.fpocket_filter import fpocket_filter
+from biobb_structure_utils.utils.extract_residues import extract_residues
+from biobb_structure_utils.utils.renumber_structure import renumber_structure
+from biobb_structure_utils.utils.extract_model import extract_model
 
-def addModelPrefix(original_path, name):
+def addPrefixToPath(original_path, prefix):
     '''
-    Adds name to original_path. For example:
+    Adds prefix to original_path file name. For example:
 
     original_path = Path('/home/user/ClusteringWF/output/step2_extract_models/output.pdb')
-    name = model1
+    prefix = model1
 
     new_path = '/home/user/ClusteringWF/output/step2_extract_models/model1_output.pdb'
 
     Inputs
     ------
 
-        original_path  (Path class from pathlib):  path to original file including filename
-        i  (int):  integer 
+        original_path  (Path class):  path to original file including filename
+        prefix                (str):  prefix string
+    
+    Outputs
+    ------- 
+
+        new_path (str): path to new file including filename with prefix
     '''
 
     filename = original_path.name
     directory = original_path.parent
 
-    new_name = name + "_" + filename
+    new_name = prefix + "_" + filename
 
     new_path = os.path.join(directory, new_name)
 
@@ -96,11 +104,15 @@ def printAvailablePockets(pockets_path, global_log):
     ------
         pockets_path  (str): Path to pockets folder including file name
         global_log (Logger): Object of class Logger (dumps info to global log file of this workflow)
+    
     Output
     ------
         Info dumped to log file
         Dictionary with models and pockets  {modelname1 : [1,2,3], modelname2 : [2,4]}
     '''
+
+    global_log.info("    Available models and pockets after filtering:")
+
     # Dictionary where pockets will be saved
     pockets_dict = {}
 
@@ -128,7 +140,7 @@ def printAvailablePockets(pockets_path, global_log):
         # Print to global log
         if modelName is not None:
 
-            global_log.info("   Model {}".format(modelName))
+            global_log.info("    Model {}".format(modelName))
 
             if pockets is not None:
 
@@ -253,14 +265,111 @@ def findNumberInString(string):
 
     return int(number)
 
+def extractPocketResidues(props, paths, pocket_residues, model):
+    '''
+    Extracts residues from PDB, returns path to the pdb file with the residues
+
+    Inputs
+    ------
+        props               (dict): properties of step
+        paths               (dict): paths of step
+        pocket_residues      (str): string with residues ID separated by white spaces
+        model                (str): pdb model name
+    
+    Output
+    ------
+        pocket_residues_path (str): path to file with residues
+
+    '''
+
+    pocket_residues = pocket_residues.split()
+
+    pocket_residues = [int(res) for res in pocket_residues]
+
+    props.update({'residues' : pocket_residues})
+
+
+    original_output_path = Path(paths['output_residues_path'])
+    original_input_path = Path(paths['input_structure_path'])
+
+    pocket_residues_path = os.path.join(str(original_output_path.parent), model + "_" + original_output_path.name)
+    input_structure_path = os.path.join(str(original_input_path.parent), model + "_" + original_input_path.name)
+
+    paths.update({'output_residues_path' : pocket_residues_path})
+    paths.update({'input_structure_path' : input_structure_path})
+    
+    extract_residues(**paths, properties=props)
+
+    return pocket_residues_path
+
+def renumberStructure(props, paths, input_structure_path, model):
+    '''
+    Renumber atoms and residues indexes
+
+    Inputs
+    ------
+        props               (dict): properties of step
+        paths               (dict): paths of step
+        input_structure_path (str): input pdb path
+        model                (str): pdb model name
+    
+    Output
+    ------
+        output_structure_path (str): path to renumbered pdb file 
+
+    '''
+
+    original_struct_path = Path(paths['output_structure_path'])
+    original_map_path = Path(paths['output_mapping_json_path'])
+
+    output_structure_path = os.path.join(str(original_struct_path.parent), model + "_" + original_struct_path.name)
+    output_mapping_json_path = os.path.join(str(original_map_path.parent), model + "_" + original_map_path.name)
+
+    paths.update({'input_structure_path' : input_structure_path})
+    paths.update({'output_structure_path' : output_structure_path})
+    paths.update({'output_mapping_json_path' : output_mapping_json_path})
+
+    renumber_structure(**paths, properties=props)
+
+    return output_structure_path
+
+def extractModel(props, paths, model):
+    '''
+    Extract model
+
+    Inputs
+    ------
+        props               (dict): properties of step
+        paths               (dict): paths of step
+        model                (str): pdb model name
+    
+    Output
+    ------
+        output_structure_path (str): path to pdb file with extracted model
+
+    '''
+
+    original_output_path = Path(paths['output_structure_path'])
+    original_input_path = Path(paths['input_structure_path'])
+
+    output_structure_path = os.path.join(str(original_output_path.parent), model + "_" + original_output_path.name)
+    input_structure_path = os.path.join(str(original_input_path.parent), model + "_" + original_input_path.name)
+
+    paths.update({'input_structure_path' : input_structure_path})
+    paths.update({'output_structure_path' : output_structure_path})
+
+    extract_model(**paths, properties=props)
+
+    return output_structure_path
+
 
 def main(args):
     
     start_time = time.time()
 
-    # Set default value for 'to_do' arg
-    if args.to_do is None:
-        args.to_do = 'all'
+    # Set default value for 'last_step' arg
+    if args.last_step is None:
+        args.last_step = 'all'
 
     # Receiving the input configuration file (YAML)
     conf = settings.ConfReader(args.main_config_path )
@@ -294,15 +403,15 @@ def main(args):
     default_summary = Path(paths_fpocket['output_summary'])
 
     # Generic name for pdb file in centroids folder
-    all_pdb_files = os.path.join(args.input_path, "*.pdb")
+    all_pdb_files = sorted(glob.glob(os.path.join(args.input_path, "*.pdb")))
 
     # Find number of pdbs in input folder
-    for model in glob.glob(all_pdb_files):
+    for model in all_pdb_files:
 
         name = Path(model).stem
 
-        zip_path = addModelPrefix(default_zip, name)
-        summary_path = addModelPrefix(default_summary, name)
+        zip_path = addPrefixToPath(default_zip, name)
+        summary_path = addPrefixToPath(default_summary, name)
 
         # Update path dictionary
         paths_fpocket.update({'input_pdb_path':model})
@@ -315,7 +424,11 @@ def main(args):
         if not validateStep(paths_fpocket["output_pockets_zip"], paths_fpocket["output_summary"]):
             global_log.info("    ERROR: no output from fpocket.")
             return 0
-    
+
+    if args.last_step == 'cavity':
+        global_log.info("Cavity analysis completed.")
+        return 
+
 # STEP 2: Filtering cavities
 
     # Write next action to global log
@@ -331,14 +444,14 @@ def main(args):
     default_zip_output = Path(paths_filter['output_filter_pockets_zip'])
 
     # For each pdb model
-    for model in glob.glob(all_pdb_files):
+    for model in all_pdb_files:
 
         name = Path(model).stem
 
         # Modify the path names according to model index
-        zip_input = addModelPrefix(default_zip_input, name)
-        summary_input = addModelPrefix(default_summary_input, name)
-        output_path = addModelPrefix(default_zip_output, name)
+        zip_input = addPrefixToPath(default_zip_input, name)
+        summary_input = addPrefixToPath(default_summary_input, name)
+        output_path = addPrefixToPath(default_zip_output, name)
 
         # Update path dictionary
         paths_filter.update({'input_pockets_zip':zip_input})
@@ -347,39 +460,89 @@ def main(args):
 
         fpocket_filter(**paths_filter, properties=prop_filter)
 
-# STEP 3: Use active ligands to screen the cavities
+    if args.last_step == 'filtering':
+        global_log.info("Cavity analysis completed.")
+        return 
 
-    # NOTE it would be nice to add a region filter...
     # Find available models with pockets after filtering
     pockets_dict = printAvailablePockets(paths_filter["output_filter_pockets_zip"], global_log)
 
-    global_log.info("step3_screen_models:")
+    # NOTE: cavity results should better inform autodock on each model 
+    # right now if there is a good pocket somewhere (doesn't matter where) we proceed to next step
+
+    # NOTE: parallelize over ligands
+
+# STEP 3: Use active ligands to screen all the filtered cavities or pocket defined by residues
+
+    global_log.info("step3_screen_models: screen pockets docking the probe ligands \n")
+
+    if args.pocket_res:
+        global_log.info("    Residues defining pocket are given: {} \n".format(args.pocket_res))
+    else:
+        global_log.info("    Residues defining pocket are not given, all pockets will be screened \n")
+
+    model_history = []
+    avg_affinity_history = []
 
     # For each model
     for model in pockets_dict.keys():
 
         # Print model name
-        global_log.info("    For model {}".format(model))
+        global_log.info(" ")
+        global_log.info("  For model **** {} **** \n".format(model))
 
-        # For each pocket
-        for pocket_ID in pockets_dict[model]:
+        # Find path to PDB model
+        input_structure_path = os.path.join(args.input_path, model + ".pdb")
+        
+        # Find path to filtered pockets
+        input_pockets_path = addPrefixToPath(default_zip_output, model)
 
-            # Find path to filtered pockets
-            input_pockets_path = addModelPrefix(default_zip_output, model)
+        # Extract residues defining the pocket if list is provided -> dock ligands to that region
+        if args.pocket_res:
 
-            # Find path to PDB model
-            input_structure_path = os.path.join(args.input_path, model + ".pdb")
+            # Renumber structure
+            global_log.info("   step3A_renumber_structure: Renumbering structure")
 
-            # Dock all ligands and save results
-            output_dir, bestAffinities, _ = docking_htvs(configuration_path = args.htvs_config_path, 
-                                                                ligand_lib_path = args.ligand_lib, 
-                                                                last_step = "all", 
-                                                                input_pockets_path = input_pockets_path, 
-                                                                pocket_ID = pocket_ID,
-                                                                input_structure_path = input_structure_path)
+            prop_renumStruct = global_prop['step3A_renumber_structure'].copy()
+            paths_renumStruct = global_paths['step3A_renumber_structure'].copy()
 
+            output_structure_path = renumberStructure(props = prop_renumStruct,
+                                                    paths = paths_renumStruct,
+                                                    input_structure_path = input_structure_path,
+                                                    model = model)
+
+            # Extract model from structure
+            global_log.info("   step3B_extract_model: Extracting model")
+
+            prop_model = global_prop['step3B_extract_model'].copy()
+            paths_model = global_paths['step3B_extract_model'].copy()
+
+            output_structure_path = extractModel(props = prop_model,
+                                                paths = paths_model,
+                                                model = model)            
+
+            # Extract residues defining pocket
+            global_log.info("   step3C_extract_residues: Extracting residues")
+
+            prop_pocketRes = global_prop['step3C_extract_residues'].copy()
+            paths_pocketRes = global_paths['step3C_extract_residues'].copy()
+
+            pocket_residues_path = extractPocketResidues(props = prop_pocketRes,
+                                                        paths = paths_pocketRes, 
+                                                        pocket_residues = args.pocket_res, 
+                                                        model = model)
+
+            # Extract residues defining pocket
+            global_log.info("   step4: Docking probe ligands")
+
+            # Dock all ligands using residues to define box
+            output_dir, bestAffinities, bestIDs, bestNames = docking_htvs(configuration_path = args.htvs_config_path, 
+                                                                        ligand_lib_path = args.ligand_lib, 
+                                                                        last_step = "all", 
+                                                                        pocket_residues_path = pocket_residues_path,
+                                                                        input_structure_path = output_structure_path)
             # New path to save docking_vs results
-            new_output_dir = os.path.join(wdir, model + "_" + str(pocket_ID) + "_VS")
+            new_output_dir = os.path.join(wdir, model + "_VS")
 
             # Move output_dir so it's not overwritten
             shutil.move(output_dir, new_output_dir)
@@ -388,10 +551,70 @@ def main(args):
             average_affinity = sum(bestAffinities)/len(bestAffinities)
 
             # Print results for this pocket in global log
-            global_log.info("        Pocket {} yields average affinity of {}".format(pocket_ID, average_affinity))
+            global_log.info("     Yields average affinity of * {} * ".format(round(average_affinity, 3)))
+
+            model_history.append(model)
+            avg_affinity_history.append(average_affinity)
 
             # Print detailed affinities per compound
-            
+            for lig_index in range(len(bestAffinities)):
+
+                if bestNames[lig_index] is None:
+                    ligand = bestIDs[lig_index]
+                else:
+                    ligand = bestNames[lig_index]
+
+                global_log.info("        Ligand {} yields affinity of {}".format(ligand, bestAffinities[lig_index]))
+
+        # If residues defining pocket are not provided -> dock ligands to all pockets found
+        else:
+
+            # For each pocket
+            for pocket_ID in pockets_dict[model]:
+
+                # Dock all ligands and save results
+                output_dir, bestAffinities, bestIDs, bestNames = docking_htvs(configuration_path = args.htvs_config_path, 
+                                                                        ligand_lib_path = args.ligand_lib, 
+                                                                        last_step = "all", 
+                                                                        input_pockets_path = input_pockets_path, 
+                                                                        pocket_ID = pocket_ID,
+                                                                        input_structure_path = input_structure_path)
+
+                # New path to save docking_vs results
+                new_output_dir = os.path.join(wdir, model + "_" + str(pocket_ID) + "_VS")
+
+                # Move output_dir so it's not overwritten
+                shutil.move(output_dir, new_output_dir)
+
+                # Average affinity
+                average_affinity = sum(bestAffinities)/len(bestAffinities)
+
+                # Print results for this pocket in global log
+                global_log.info("        Pocket {} yields average affinity of {}".format(pocket_ID, round(average_affinity, 3)))
+
+                # Print detailed affinities per compound
+                for lig_index in range(len(bestAffinities)):
+
+                    if bestNames[lig_index] is None:
+                        ligand = bestIDs[lig_index]
+                    else:
+                        ligand = bestNames[lig_index]
+
+                    global_log.info("            Ligand {} yields affinity of {} \n".format(ligand, round(bestAffinities[lig_index]),3))
+    
+    if args.pocket_res:
+
+        # Print ranking of models
+        avg_affinity_history, model_history = zip(*sorted(zip(avg_affinity_history, model_history)))
+
+        global_log.info(" ")
+        global_log.info("*** FINAL RANKING ***")
+
+        for model_index in range(len(model_history)):
+
+            global_log.info("  Model {} gives average affinity of: {}".format(model_history[model_index], round(avg_affinity_history[model_index], 3)))
+
+
     # Print timing information to the log file
     elapsed_time = time.time() - start_time
     global_log.info('')
@@ -402,6 +625,8 @@ def main(args):
     global_log.info('')
     global_log.info('Elapsed time: %.1f minutes' % (elapsed_time/60))
     global_log.info('')
+
+    return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Simple clustering, cavity analysis and docking pipeline using BioExcel Building Blocks")
@@ -418,13 +643,17 @@ if __name__ == '__main__':
                         help="Path to folder with pdb models (For example PDB centroids from clustering an MD trajectory)", 
                         required=True)
 
-    # Execute workflow until 'to_do' step -> all executes all steps (all is default)
-    parser.add_argument('--until', dest='to_do', 
-                        help="(Opt, default: all) Extent of the pipeline to execute (cavity, all)", 
+    # Execute workflow until 'last_step' step -> all executes all steps (all is default)
+    parser.add_argument('--until', dest='last_step', 
+                        help="(Opt, default: all) Extent of the pipeline to execute (cavity, filtering, all)", 
                         required=False)
 
     parser.add_argument('--lig-lib', dest='ligand_lib',
                         help="Path to file with ligand library. The file should contain one ligand identifier (Ligand PDB code, SMILES or Drug Bank ID) per line.",
+                        required=False)
+
+    parser.add_argument('--pocket-res', dest='pocket_res',
+                        help="List of residue IDs defining the pocket. If provided, the ligands will be docked in a box covering this region, ignoring pockets found.",
                         required=False)
 
     args = parser.parse_args()
