44fb4382b960b604e43fe8e23f44e1220908e4eb
PabloNA97 <p.navarro.acero@gmail.com>
2023-04-05 10:32:43 +0200

    Added flag

--- OTHER/4.Pocket_VS/run_local.sh
+++ OTHER/4.Pocket_VS/run_local.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # MODELS_PATH=/home/nbd-pablo/Documents/2022_ENSEM/3.P53/MD_Trajectories/3ts8/CLUSTER/selection2/low_num_clusters/MDTRAJ_HIERA/clustering  
-MODELS_PATH=/home/nbd-pablo/Documents/2022_ENSEM/3.P53/MD_Trajectories/pocket_vs_results/best_affinities_from_trajs/clustering/cpptraj_clust/10_clusters/representative
+MODELS_PATH=/home/nbd-pablo/Documents/2023_EUCANSHARE/Javi_pdbs
 
 LIGAND_LIB_PATH=/home/nbd-pablo/repos/biobb_workflows/OTHER/4.Pocket_VS/input/UCI_subset_reduced.smi
 
-POCKET_RESIDUES='19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 47 48 49 50 51'
+POCKET_RESIDUES='19 20 21 22'
 
-python biobb_pocket_vs.py --main-config input.yml --cavity-config cavity_input.yml --htvs-config htvs_input.yml --lig-lib $LIGAND_LIB_PATH --input $MODELS_PATH --pocket-res "$POCKET_RESIDUES" --until all
+python biobb_pocket_vs.py --main-config input.yml --cavity-config cavity_input.yml --htvs-config htvs_input.yml --lig-lib $LIGAND_LIB_PATH --input $MODELS_PATH --pocket-res "$POCKET_RESIDUES" --until filtering 
 
 # python biobb_pocket_vs.py --main-config input.yml --htvs-config htvs_input.yml --lig-lib $LIGAND_LIB_PATH --input $MODELS_PATH --until all
 
