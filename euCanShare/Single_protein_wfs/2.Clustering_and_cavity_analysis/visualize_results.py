dfec47c1d25aeccdb8a4f57ad2c70f6a86203cfe
PabloNA97 <p.navarro.acero@gmail.com>
2023-09-06 18:18:31 +0200

    Added optional summary sorting after the WF has finished

--- euCanShare/Single_protein_wfs/2.Clustering_and_cavity_analysis/visualize_results.py
+++ euCanShare/Single_protein_wfs/2.Clustering_and_cavity_analysis/visualize_results.py
@@ -5,10 +5,16 @@
 import os
 import pymol 
 import glob
+import yaml
 import zipfile
 import logging
 import regex as re
 from pathlib import Path
+
+
+#############
+# Functions #
+#############
 
 def load_model(model_name, model_path, models_cartoon_color, residues_to_highlight):
 
@@ -39,6 +45,90 @@ def load_model(model_name, model_path, models_cartoon_color, residues_to_highlig
 
     return
 
+def sort_pockets(pockets_summary_path: str, output_folder: str):
+    """
+    Function that reads the YAML summary file with all models and pockets and sorts the models by:
+
+        1. Volume of the largest pocket in that model
+        2. Druggability score of the best pocket in that model
+        3. Score of the best pocket in that model
+    
+    It then writes new YAML files with the sorted models and their pockets.
+    """
+
+    # Find the YAML summary file name
+    pockets_summary_filename = Path(pockets_summary_path).stem
+
+    # Read the YAML summary file
+    with open(pockets_summary_path, "r") as f:
+        pockets_summary = yaml.load(f, Loader = yaml.FullLoader)
+
+    # Sort the pockets by volume
+    sorted_pockets = dict(sorted(pockets_summary.items(), key = lambda x: largest_volume(x[1]), reverse = True))
+
+    # Write the sorted pockets to a new YAML file
+    with open(f"{output_folder}/{pockets_summary_filename}_by_volume.yml", "w") as f:
+        yaml.dump(sorted_pockets, f, sort_keys = False)
+
+    # Sort the pockets by druggability score
+    sorted_pockets = dict(sorted(pockets_summary.items(), key = lambda x: highest_drug_score(x[1]), reverse = True))
+
+    # Write the sorted pockets to a new YAML file 
+    with open(f"{output_folder}/{pockets_summary_filename}_by_drug_score.yml", "w") as f:
+        yaml.dump(sorted_pockets, f, sort_keys = False)
+
+    # Sort the pockets by score
+    sorted_pockets = dict(sorted(pockets_summary.items(), key = lambda x: highest_score(x[1]), reverse = True))
+
+    # Write the sorted pockets to a new YAML file
+    with open(f"{output_folder}/{pockets_summary_filename}_by_score.yml", "w") as f:
+        yaml.dump(sorted_pockets, f, sort_keys = False)
+    
+    return
+
+def largest_volume(model: dict):
+    """
+    Function to sort the pockets by volume
+    """
+
+    # Find the largest volume
+    largest_volume = 0
+    for pocket in model["pockets"]:
+        if model[pocket]["volume"] > largest_volume:
+            largest_volume = model[pocket]["volume"]
+
+    return largest_volume
+
+def highest_drug_score(model: dict):
+    """
+    Function to sort the pockets by druggability score
+    """
+
+    # Find the highest druggability score
+    highest_drug_score = 0
+    for pocket in model["pockets"]:
+        if model[pocket]["druggability_score"] > highest_drug_score:
+            highest_drug_score = model[pocket]["druggability_score"]
+
+    return highest_drug_score
+
+def highest_score(model: dict):
+    """
+    Function to sort the pockets by score
+    """
+
+    # Find the highest score
+    highest_score = 0
+    for pocket in model["pockets"]:
+        if model[pocket]["score"] > highest_score:
+            highest_score = model[pocket]["score"]
+
+    return highest_score
+
+#############
+# Constants #
+#############
+
 # Define constants
 LOG_FILENAME = "visualize_results.log"
 
@@ -68,10 +158,15 @@ logger.info("==============\n")
 ##########
 
 # Change to the output folder of the cavity analysis
-output_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round3_new/clustering/extended_pocket/states/pocket_analysis"
+#output_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round3_new/clustering/extended_pocket/states/pocket_analysis_extendedRdown"
+output_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round0/trajectories/clustering/extended_pocket/states/pocket_analysis_allstate9_vol300_ds3_sc3"
 
 # Change to the input folder of the cavity analysis or path were the models are stored
-input_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round3_new/clustering/extended_pocket/states/input"
+# input_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round3_new/clustering/extended_pocket/states/input/extended_R_down"
+input_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/round0/trajectories/clustering/extended_pocket/states/input/all_state9"
+
+# Summary file with all the pockets
+pockets_summary_path = f"{output_folder}/pocket_analysis.yml"
 
 # Experimental structure to compare the models to
 experimental_folder = "/home/nbd-pablo/Documents/2023_ENSEM/P53/Crystals_and_models/prepared"
@@ -82,6 +177,9 @@ experimental_colors.extend(experimental_colors)
 
 # Wether to show all the models or just the ones with pockets
 show_all_models = False
+
+# Wether to sort the pockets by volume, druggability score and score or not
+sort_pockets_summary = True
 
 # Distance to the pocket to show the atoms explicitly
 distance_to_pocket = 8
@@ -130,6 +228,10 @@ pockets_folders = [f"{pocket_folder}{filtered_pockets_folder}/" for pocket_folde
 
 # Add filtered pockets filename to each path
 filtered_pockets_paths = [f"{pocket_folder}{filtered_pockets_filename}" for pocket_folder in pockets_folders]
+
+# Sort pockets
+if sort_pockets_summary:
+    sort_pockets(pockets_summary_path, output_folder)
 
 ##########################
 # Unzip filtered pockets #
@@ -228,7 +330,8 @@ for model_path in model_paths:
             pymol.cmd.delete(model_name)
             
             # Align temporal object to reference 
-            pymol.cmd.align(f"{model_name}_and_pockets", reference_model)
+            if model_name != reference_model:
+                pymol.cmd.align(f"{model_name}_and_pockets", reference_model)
 
             # Extract the pocket  
             pymol.cmd.extract(f"{pocket_objects[-1]}", f"{model_name}_and_pockets and resname STP")
@@ -278,11 +381,13 @@ for model_index, model_path in enumerate(experimental_paths):
     # Set a random color to the model
     pymol.cmd.color(experimental_colors[model_index], model_name)
 
+
+#####################
+# General settings  #
+#####################
+
 # Hide all hydrogens (hide licorice, hydrogens)
 pymol.cmd.hide("licorice", "hydrogens")
-
-# Hide reference model
-pymol.cmd.hide("everything", reference_model)
 
 # Set min_mesh_spacing to 0.4
 pymol.cmd.set("min_mesh_spacing", 0.4)
